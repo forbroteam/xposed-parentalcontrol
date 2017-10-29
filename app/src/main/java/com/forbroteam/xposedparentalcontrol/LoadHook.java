6969b88d6e2fd9d802ad853b90f77c468429cbdf
Dmitri Bogatenkov <dmitri.bogatenkov@pocopay.com>
2017-10-29 12:28:04 +0000

    Add url check

--- app/src/main/java/com/forbroteam/xposedparentalcontrol/LoadHook.java
+++ app/src/main/java/com/forbroteam/xposedparentalcontrol/LoadHook.java
@@ -1,6 +1,8 @@
 package com.forbroteam.xposedparentalcontrol;
 
+import android.app.ActivityManager;
 import android.app.AndroidAppHelper;
+import android.content.Context;
 import android.widget.Toast;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -25,18 +27,19 @@ public class LoadHook implements IXposedHookLoadPackage {
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         packageName = lpparam.packageName;
 
-        XposedBridge.log("Loaded app: " + packageName);
+        XposedBridge.log("ParentalControl: Loaded " + packageName);
         if (!targetPackage.isEmpty() && !targetPackage.equals(packageName))
             return;
 
         findAndHookConstructor("java.net.URL", lpparam.classLoader, String.class,
                 new XC_MethodHook() {
-
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedBridge.log("ParentalControl: " + packageName + " URL param 0: " + param.args[0] +
-                                " URL param 1: " + param.args[1]);
-                        if (param.args[0].toString().contains("youtube")) {
+                        XposedBridge.log("ParentalControl: " + packageName + " URL param 0: " + param.args[0]);
+                        String url = (param != null && param.args.length > 0) ? ((String) param.args[0]) : null;
+                        XposedBridge.log("ParentalControl: ");
+                        XposedBridge.log("ParentalControl: " + packageName + " URL: " + url);
+                        if (url.toString().contains("youtube")) {
                             forceClose = true;
                         }
                     }
@@ -44,11 +47,14 @@ public class LoadHook implements IXposedHookLoadPackage {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         if (forceClose) {
-                            AndroidAppHelper.currentApplication().onTerminate();
                             Toast.makeText(AndroidAppHelper.currentApplication(),
                                     "Sorry, you are restricted to be here!", Toast.LENGTH_SHORT)
                                     .show();
                             forceClose = false;
+
+                            ActivityManager am = (ActivityManager) AndroidAppHelper
+                                    .currentApplication().getSystemService(Context.ACTIVITY_SERVICE);
+                            am.killBackgroundProcesses(targetPackage);
                         }
                     }
                 });
